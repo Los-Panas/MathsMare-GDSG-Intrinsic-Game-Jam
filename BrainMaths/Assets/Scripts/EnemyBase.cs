60b58b0968c10911e03a4df6bb240983bc72cf19
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-12-05 21:32:43 +0100

    added enemy base class and override basics

---
+++ BrainMaths/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    [Header("Base")]
+    public Vector2 mPosition;
+    public Vector2 mVelocity;
+    [Header("Speed mult. factor")]
+    public Vector2 mSpeed; // Used as multipler: stores speed mult independently for each component
+    public bool randomizeStartingSpeed;
+    public Vector2 minAndMax;
+
+    public float minCoordToDestroy = 0.0f; // coords for the left side to despawn the enemy, to adjust sprite size offset
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPosition = transform.position;
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckOffScreenConditions();
+        Move();        
+    }
+
+    private void CheckOffScreenConditions()
+    {
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if(screenPosition.x < minCoordToDestroy)
+        {
+            Debug.Log("TODO: Oriorii I need to destroy myself");
+        }
+    }
+
+    public virtual void Initialize()
+    {
+        if (randomizeStartingSpeed)
+        {
+            mSpeed.x = Random.Range(minAndMax.x, minAndMax.y);
+            mSpeed.y = Random.Range(minAndMax.x, minAndMax.y);
+        }
+        /*else
+        {
+            mSpeed = new Vector2(1.0f, 1.0f);
+        }*/
+    }
+
+    public virtual void Move()
+    {
+        mPosition.x += mVelocity.x * mSpeed.x * Time.deltaTime;
+        transform.position = mPosition;  
+    }
+}
---
+++ BrainMaths/Assets/Scripts/EnemySin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySin : EnemyBase
+{
+    [Header("EnemySin")]
+    public float amplitude = 2.0f;
+    public float waveSpeed = 10.0f;  // TODO: speed based on bpm?
+    public float mySpeedX = 3.0f;
+    private float value;
+    public override void Initialize()
+    {
+        Debug.Log("Im enemy moUstachoe, and i dont want random speed");
+        mSpeed = new Vector2(mySpeedX, waveSpeed);
+    }
+
+    public override void Move()
+    {
+        base.Move(); // always move on x
+
+        mSpeed.y = waveSpeed; // for runtime mod
+        value += mSpeed.y * Time.deltaTime;
+
+        Vector2 pos_y = transform.position;
+        pos_y.y = Mathf.Sin(value) * amplitude;
+
+        transform.position = pos_y;
+    }
+}
