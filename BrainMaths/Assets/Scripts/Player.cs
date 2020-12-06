06e03d0255db276e10bf7cb7c7bc99b45fb922f5
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-12-06 16:33:17 +0100

    New Player movement, Now Player working with physics, and some fixed

--- BrainMaths/Assets/Scripts/Player.cs
+++ BrainMaths/Assets/Scripts/Player.cs
@@ -23,28 +23,26 @@ public class Player : MonoBehaviour
         ULTRA_A = 13
     }
 
-    enum GravityState
-    {
-        Normal,
-        Floating
-    }
-
     // Inspector -------------------
     [Header("Player Variables")]
     [SerializeField]
-    float acceleration = 0.5f;
-    [SerializeField]
-    float max_velocity = 5.0f;
+    float speed = 5.0f;
     [SerializeField]
     float sinus_speed = 2.0f;
+    [SerializeField]
+    bool is_invulnerable = false;
+    [SerializeField]
+    float time_to_hold_special = 0.25f;
+
+
+    [Header("Invulnerability Animation")]
     [SerializeField]
     int invulnerable_cycles = 3;
     [SerializeField]
     float invulnerable_time = 2;
     [SerializeField]
     Color invulnerable_color;
-    [SerializeField]
-    bool is_invulnerable = false;
+
 
     [Header("GameObjects / Components")]
     [SerializeField]
@@ -68,24 +66,24 @@ public class Player : MonoBehaviour
     int enemyCount = 0;
     [SerializeField]
     int[] enemyCoutToUpgrade;
-    Coroutine barCoroutine = null;
-    float auxCountBar = 0;
-    // -----------------------------
 
     [Header("Controls")]
     [SerializeField]
-    KeyCode changeGravity = KeyCode.W;
+    KeyCode MoveUp = KeyCode.W;
     [SerializeField]
-    KeyCode specialAttack = KeyCode.E;
+    KeyCode MoveDown = KeyCode.S;
     [SerializeField]
     float timeAfterTouchBounds = 0;
+    // -----------------------------
 
     // Internal Variables ----------
-    GravityState g_state = GravityState.Normal;
+    Rigidbody2D rb;
+    Coroutine barCoroutine = null;
+    float auxCountBar = 0;
     float current_velocity = 0.0f;
     int current_gravity_direction = -1;
     Vector3 pos;
-    float time_hold = 0.0f;
+    float special_time_hold = 0.0f;
     float manual_time = 0;
     bool special_charged = false;
     Coroutine grade_perlin_noise_coroutine;
@@ -97,6 +95,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         instance = this;
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
 
         pos = transform.position;
         AddGrade();
@@ -106,49 +106,6 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (barCoroutine == null && auxCountBar != 0.0F)
-        {
-            barCoroutine = StartCoroutine(BarUp(auxCountBar));
-            auxCountBar = 0.0F;
-        }
-
-        HandleInput();
-
-        if (g_state == GravityState.Normal)
-        {
-            // Gravity -> I use deltaTime 2 times because both acceleration and velocity need to be converted to per frame instead of per second.
-            current_velocity += acceleration * current_gravity_direction;  // v = v0 + a*t 
-            current_velocity = Mathf.Clamp(current_velocity, -max_velocity, max_velocity);
-            pos.y += current_velocity * Time.deltaTime; // x = x0 + v*t
-            transform.position = pos;
-            // ------------------------------------------------------------------------------
-        }
-        else
-        {
-            // Sinus Movement while Holding
-            manual_time += Time.deltaTime * sinus_speed;
-
-            if (current_velocity != 0)
-            {
-                current_velocity -= acceleration * 0.5f * current_gravity_direction;
-                pos.y += current_velocity * Time.deltaTime;
-
-                if (current_velocity < 0.1f && current_velocity > -0.1f)
-                {
-                    current_velocity = 0;
-                }
-            }
-
-            pos.y += Mathf.Sin(manual_time) * Time.deltaTime * 0.35f;
-            transform.position = pos;
-            // ------------------------------------------------------------------------------
-        }
-
-        //Analysis();
-    }
-
-    void HandleInput()
     {
         // TODO: Remove
         // Debug --------------------------------
@@ -163,91 +120,66 @@ public class Player : MonoBehaviour
         }
         // ---------------------------------------
 
-        if (CanUserChangeGravity())
+        manual_time += Time.deltaTime * sinus_speed;
+
+        if (barCoroutine == null && auxCountBar != 0.0F)
         {
-            if (Input.GetKey(changeGravity))
-            {
-                time_hold += Time.deltaTime;
-
-                if (time_hold >= 0.15f && g_state != GravityState.Floating)
-                {
-                    g_state = GravityState.Floating;
-
-                    if (current_gravity_direction < 0)
-                    {
-                        manual_time = Mathf.PI;
-                    }
-                    else
-                    {
-                        manual_time = 0;
-                    }
-                }
-            }
-
-            if (Input.GetKeyUp(changeGravity))
-            {
-                ChangeGravityDirection();
-
-                time_hold = 0;
-            }
-        }
-
-        if (Input.GetKeyDown(specialAttack))
-        {
-            if (special_charged)
-            {
-                UseSpecial();
-            }
-            else
-            {
-                if (grade_perlin_noise_coroutine == null)
-                {
-                    grade_perlin_noise_coroutine = StartCoroutine(NoSpecialChargedFeedback());
-                }
-            }
+            barCoroutine = StartCoroutine(BarUp(auxCountBar));
+            auxCountBar = 0.0F;
         }
     }
 
-    void ChangeGravityDirection()
+    private void FixedUpdate()
     {
-        if (g_state == GravityState.Floating)
-        {
-            if (Mathf.Sin(manual_time) > 0)
-            {
-                current_gravity_direction = 1;
-            }
-            else
-            {
-                current_gravity_direction = -1;
-            }
+        HandleInput();
+    }
 
-            g_state = GravityState.Normal;
+    void HandleInput()
+    {
+        Vector2 movement = Vector2.zero;
+
+        if (Input.GetKey(MoveUp) && Input.GetKey(MoveDown))
+        {
+            special_time_hold += Time.deltaTime;
+
+            if (special_time_hold >= time_to_hold_special)
+            {
+                if (special_charged)
+                {
+                    UseSpecial();
+                }
+                else
+                {
+                    if (grade_perlin_noise_coroutine == null)
+                    {
+                        grade_perlin_noise_coroutine = StartCoroutine(NoSpecialChargedFeedback());
+                    }
+                }
+
+                special_time_hold = 0;
+            }
+        }
+        else if(!Input.GetKey(MoveUp) && !Input.GetKey(MoveDown))
+        {
+            movement.y += Mathf.Sin(manual_time) * 0.35f;
         }
         else
         {
-            current_gravity_direction = -current_gravity_direction;
-        }
-    }
+            special_time_hold = 0;
 
-    bool CanUserChangeGravity()
-    {
-        if (ignoreChange)
-        {
-            if (Time.time - timeTouchBounds < timeAfterTouchBounds)
+            if (Input.GetKey(MoveUp))
             {
-                return false;
+                movement.y += speed;
             }
-            ignoreChange = false;
-        }
-        return true;
-    }
 
-    void Analysis()
-    {
-        Debug.Log("Analysis:\n" +
-            "Current Velocity: " + current_velocity +
-            "\n---------------------"
-            );
+            if (Input.GetKey(MoveDown))
+            {
+                movement.y -= speed;
+            }
+
+        }
+
+        rb.velocity = movement;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -259,71 +191,6 @@ public class Player : MonoBehaviour
                 DecreaseGrade();
                 StartCoroutine(InvulnerableAnim());
             }
-        }
-
-        if (collision.tag == "VerticalLimitUp")
-        {
-            timeTouchBounds = Time.time;
-            ignoreChange = true;
-            if (!is_invulnerable)
-            {
-                DecreaseGrade();
-                StartCoroutine(InvulnerableAnim());
-            }
-            if (g_state == GravityState.Floating)
-            {
-                g_state = GravityState.Normal;
-            }
-            current_gravity_direction = -1;
-        }
-        else if (collision.tag == "VerticalLimitDown")
-        {
-            timeTouchBounds = Time.time;
-            ignoreChange = true;
-            if (!is_invulnerable)
-            {
-                DecreaseGrade();
-                StartCoroutine(InvulnerableAnim());
-            }
-            if (g_state == GravityState.Floating)
-            {
-                g_state = GravityState.Normal;
-            }
-            current_gravity_direction = 1;
-        }
-    }
-
-    public void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag == "VerticalLimitUp")
-        {
-            timeTouchBounds = Time.time;
-            ignoreChange = true;
-            if (!is_invulnerable)
-            {
-                DecreaseGrade();
-                StartCoroutine(InvulnerableAnim());
-            }
-            if (g_state == GravityState.Floating)
-            {
-                g_state = GravityState.Normal;
-            }
-            current_gravity_direction = -1;
-        }
-        else if (collision.tag == "VerticalLimitDown")
-        {
-            timeTouchBounds = Time.time;
-            ignoreChange = true;
-            if (!is_invulnerable)
-            {
-                DecreaseGrade();
-                StartCoroutine(InvulnerableAnim());
-            }
-            if (g_state == GravityState.Floating)
-            {
-                g_state = GravityState.Normal;
-            }
-            current_gravity_direction = 1;
         }
     }
 
