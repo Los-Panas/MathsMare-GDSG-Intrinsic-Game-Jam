c5b8d8ea626fcf768e48c346a1f2e6e912c559d9
peterMcP <36275626+peterMcP@users.noreply.github.com>
2020-12-08 19:10:21 +0100

    added first approach to enemy beat change color

---
+++ BrainMaths/Assets/Scripts/EnemyChangeColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChangeColor : MonoBehaviour
+{
+    public SpriteRenderer[] srs;
+    
+
+    public void ChangeColor(Color[] allColors)
+    {
+
+        int n_color = Random.Range(0, allColors.Length);
+
+        while(allColors[n_color] == Camera.main.backgroundColor)
+        {
+            n_color = Random.Range(0, allColors.Length);
+        }
+
+        for (int i = 0; i < srs.Length; ++i)
+        {
+            srs[i].color = allColors[n_color];
+        }
+    }
+}
